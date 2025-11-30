02062786480e41bd75252fc961f810de600dfc68
Ajay Meena <ramsevakmeena93@gmail.com>
2025-11-30 19:42:06 +0530

    Improve PWA installability - enable Install App in Chrome

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,56 @@
-const CACHE_NAME = 'ayutrace-v1';
+const CACHE_NAME = 'ayutrace-v2';
 const urlsToCache = [
   '/ayutrace/',
   '/ayutrace/index.html',
+  '/ayutrace/manifest.json',
+  '/ayutrace/icon.svg',
 ];
 
+// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
   );
 });
